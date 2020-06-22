131da8d9d0972b2d65aaf92731cbbff110b52ba0
mahanmmi <mahanmmi@gmail.com>
2020-06-22 12:49:04 +0430

    created project and app for django

---
+++ profiles-rest-api/profile-rest-api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mahanmmi/mahanmmi/Projects/Web/Back-End/Django-Learning/profiles-rest-api/profile-rest-api/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
